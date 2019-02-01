2ea85a90e5ed5f863b46306db0680861503c5f8d
Florentino G O Silva <florentinogomes@gmail.com>
2019-02-01 15:37:56 +0100

    fill lists

--- print_func.py
+++ print_func.py
@@ -1,12 +1,21 @@
 #
 import sys
-
+import numpy as np
 
 if( len(sys.argv)!=2):
 	print("No input parameter...")
 	print("Must input an integer.")
 	sys.exit()
 
+x=[];y=[]
+for i in range (0,101):
+	ii= -5.0 + 0.1 * i
+	x.append(ii)
 
+if (int(sys.argv[1])==1):
+	y=x
+	print(y)
+else:
+	print("not one")
 	
 
