0c2c4d50521ec664f2b094f279223695d28b2002
Florentino G O Silva <florentinogomes@gmail.com>
2019-02-01 15:40:19 +0100

    plot lists

--- print_func.py
+++ print_func.py
@@ -1,21 +1,24 @@
 #
 import sys
 import numpy as np
+import matplotlib.pyplot as mplot
 
 if( len(sys.argv)!=2):
 	print("No input parameter...")
 	print("Must input an integer.")
 	sys.exit()
 
-x=[];y=[]
+xvar=[];yvar=[]
 for i in range (0,101):
 	ii= -5.0 + 0.1 * i
-	x.append(ii)
+	xvar.append(ii)
 
 if (int(sys.argv[1])==1):
-	y=x
-	print(y)
+	yvar=xvar
+	#print(yvar)
+	mplot.plot(xvar,yvar)
+	mplot.show()
 else:
-	print("not one")
+	print("Not one")
 	
 
