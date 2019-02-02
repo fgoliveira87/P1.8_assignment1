43bd24e1bac928ec066c251312090d5011e06046
Florentino G O Silva <florentinogomes@gmail.com>
2019-02-03 00:04:38 +0100

    fill lists

--- print_func.py
+++ print_func.py
@@ -1,6 +1,16 @@
 #
 import sys
+import numpy as np
 
 if( len(sys.argv)!=2):
 	print("No input parameter...")
 	sys.exit()
+	
+xvar=[];yvar=[]
+for i in range (0,101):
+	ii= -5.0 + 0.1 * i
+	xvar.append(ii)
+	
+if (int(sys.argv[1])==1):
+	yvar=xvar
+print (yvar)
